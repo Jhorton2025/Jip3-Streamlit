bc707ca07458e12aa929afd50cae9def5475e5e2
Jhorton2025 <138929160+Jhorton2025@users.noreply.github.com>
2023-07-14 15:51:13 -0400

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
+st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
 st.write("Here is the Jip3 chatbot link [link](https://filegpt.app/chatbot?collection=clk0scnx30002mj0fr9ii685r)")
