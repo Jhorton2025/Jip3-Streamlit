7118e01d0fb07123d355193c92855c0b2c5e5488
Jhorton2025 <138929160+Jhorton2025@users.noreply.github.com>
2023-07-18 21:40:35 -0400

    Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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).")
-st.write("Here is the MAPK8IP3 chatbot link [link](https://www.filegpt.app/chatbot?collection=clk75mzfw0002ky0fn395k7x4).")
+st.write("Here is the pubmed chatbot [link](https://sightbot.brilliantly.ai?key=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).")
+st.write("Here is the MAPK8IP3 chatbot [link](https://www.filegpt.app/chatbot?collection=clk75mzfw0002ky0fn395k7x4).")
